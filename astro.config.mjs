b952e5dccde198e699462871e0902385fc6829d6
iamcos <49107951+iamcos@users.noreply.github.com>
2025-10-15 17:55:15 +0700

    Fix Astro configuration: use server output with Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rediant.art',
   base: '/',
-  output: 'hybrid',
+  output: 'server',
   integrations: [
     tailwind(),
     sitemap(),
@@ -18,7 +18,7 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
-    // vercel(),
+    vercel(),
   ],
   image: {
     domains: ['rediant.art'],
